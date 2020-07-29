13f22ef9eb060f186c5e969c8b6f794e31a9e975
Muhammad Ahmed <ahmedcs2600@gmail.com>
2020-07-30 03:19:04 +0500

    first commit

---
+++ src/main/java/com/example/democrud/DemocrudApplication.java
@@ -0,0 +1,11 @@
+package com.example.democrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemocrudApplication.class, args);
+    }
+}
